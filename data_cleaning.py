60523ff9d50e2216dfa24c007dfa13d9d38d61aa
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-01-20 21:20:55 +0530

    mdae the model

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,37 +5,22 @@ Created on Thu Jan 17 21:25:35 2019
 @author: Divyansh
 """
 import re
-import string
 
-contractions = {
-"ain't": "am not",
-"aren't": "are not",
-"can't": "cannot",
-"can't've": "cannot have",
-"'cause": "because",
-"could've": "could have",
-"couldn't": "could not",
-"couldn't've": "could not have",
-"didn't": "did not",
-"doesn't": "does not",
-"don't": "do not",
-"hadn't": "had not",
-"hadn't've": "had not have",
-"hasn't": "has not",
-"haven't": "have not",
-"he'd": "he would",
-"he'd've": "he would have",
-}
-
-def clean_text(text):
-    text = text.lower()
-    for i in contractions:
-        if i in text:
-            text = re.sub(i, contractions[i], text)
-    punctuations = list(string.punctuation)                
-    text = [i for i in text if i not in punctuations]                
-    text = "".join(text)
-    # joining and splitting have been done 2 times because the places from where the 
-    # punctutations were removed , leave an empty space out there, thus we clear it.
-    text = text.split()
-    return " ".join(text)
+def preprocess_sentence(w):
+    w = w.lower().strip()
+    
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ." 
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+    
+    w = w.rstrip().strip()
+    
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
--- data_pre_process.py
+++ data_pre_process.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import pickle
 import os 
-from data_cleaning import clean_text
+import tensorflow as tf
+from data_cleaning import preprocess_sentence
 
 ## defining constants and paths
 news_articles_path = 'data/News/'
@@ -35,89 +36,100 @@ make_summaries(news_summaries_path)
 #print(summaries)
 
 # Cleaning the news and summaries
-clean_news = [clean_text(news) for news in news_articles]
-clean_summaries = [clean_text(summary) for summary in summaries]
+clean_news = [preprocess_sentence(news) for news in news_articles]
+clean_summaries = [preprocess_sentence(summary) for summary in summaries]
+
+# 1. Remove the accents
+# 2. Clean the sentences
+# 3. Return word pairs in the format: [ENGLISH, SPANISH]
+def create_dataset():    
+    word_pairs = [[news, summaries]  for news, summaries in zip(clean_news, clean_summaries)]
+    return word_pairs[:30000]
+	
+pairs = create_dataset()
+df = pd.DataFrame({
+	'news': [news for news, summaries in pairs],
+	'summaries': [summaries for news, summaries in pairs]
+})
+
+df.to_csv('dataset_summary.csv')
 
 
-# Creating a dictionary that maps each word with its frequency
-word2count = {}
-for news, summary in zip(clean_news, clean_summaries):
-    for word in news.split():
-        if word not in word2count:
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
-    for word in summary.split():
-        if word not in word2count:
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
-
-
-# Creating dictionaries that maps each word of questions and answers with a unique integer
-# The Word2count dict contains the words and their frequency , so we will only keep the words which
-# appear a certain number of times.
-MIN_WORD_FREQUENCY = 10
-word_number = 0
-newswords2int = {}
-for word, count in word2count.items():
-    if count >= MIN_WORD_FREQUENCY:
-        newswords2int[word] = word_number
-        word_number += 1
-word_number = 0
-summarywords2int = {}
-for word, count in word2count.items():
-    if count >= MIN_WORD_FREQUENCY:
-        summarywords2int[word] = word_number
-        word_number += 1
-
-# Adding special tokens into the dict - <EOS> for end of string , <SOS> for start of string
-# <PAD> for user input , <OUT> for filtered out words
-tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
-for token in tokens:
-    newswords2int[token] = len(newswords2int) + 1
-for token in tokens:
-    summarywords2int[token] = len(summarywords2int) + 1
-
-# Creating the inverse dictionary of the answerswords2int dict
-summaryints2words = {w_i: w for w, w_i in summarywords2int.items()}
-
-# Adding the <SOS> and <EOS> at the start and end of each string respectively in the 
-# clean_answers list as, this is the target .
-for i in range(len(clean_summaries) - 1):
-    clean_summaries[i] = clean_summaries[i] + ' <EOS>'
+# This class creates a word -> index mapping (e.g,. "dad" -> 5) and vice-versa 
+# (e.g., 5 -> "dad") for each language,
+class LanguageIndex():
+  def __init__(self, lang):
+    self.lang = lang
+    self.word2idx = {}
+    self.idx2word = {}
+    self.vocab = set()
     
-# Translating all the questions and answers into their respective unique integers.
-# and Replacing the words which were filtered out by value of '<OUT>'
-news_into_ints = []
-for news in clean_news:
-    ints = []
-    for word in news.split():
-        if word not in newswords2int:
-            ints.append(newswords2int['<OUT>'])
-        else:
-            ints.append(newswords2int[word])
-    news_into_ints.append(ints)
-summary_into_ints = []
-for summary in clean_summaries:
-    ints = []
-    for word in summary.split():
-        if word not in summarywords2int:
-            ints.append(summarywords2int['<OUT>'])
-        else:
-            ints.append(summarywords2int[word])
-    summary_into_ints.append(ints)
+    self.create_index()
     
-# Sorting questions and answers by their lenghts to optimise the traning process
-sorted_clean_news = []
-sorted_clean_summaries = []
-for length in range(1, len(news_into_ints) - 1):
-    for i in enumerate(news_into_ints):
-        if len(i[1]) == length:
-            sorted_clean_news.append(news_into_ints[i[0]])
-            sorted_clean_summaries.append(summary_into_ints[i[0]])
+  def create_index(self):
+    for phrase in self.lang:
+      self.vocab.update(phrase.split(' '))
+    
+    self.vocab = sorted(self.vocab)
+    
+    self.word2idx['<pad>'] = 0
+    for index, word in enumerate(self.vocab):
+      self.word2idx[word] = index + 1
+    
+    for word, index in self.word2idx.items():
+      self.idx2word[index] = word
+	  
+def max_length(tensor):
+    return max(len(t) for t in tensor)
 
 
+def load_dataset():
+    # creating cleaned input, output pairs
+    pairs = create_dataset()
 
+    # index language using the class defined above    
+    inp_lang = LanguageIndex(news for news, summary in pairs)
+    targ_lang = LanguageIndex(summary for news, summary in pairs)
+    
+    # Vectorize the input and target languages
+    
+    # Spanish sentences
+    input_tensor = [[inp_lang.word2idx[s] for s in news.split(' ')] for news, summary in pairs]
+    
+    # English sentences
+    target_tensor = [[targ_lang.word2idx[s] for s in summary.split(' ')] for news, summary in pairs]
+    
+    # Calculate max_length of input and output tensor
+    # Here, we'll set those to the longest sentence in the dataset
+    max_length_inp, max_length_tar = max_length(input_tensor), max_length(target_tensor)
+    
+    # Padding the input and output tensor to the maximum length
+    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor, 
+                                                                 maxlen=max_length_inp,
+                                                                 padding='post')
+    
+    target_tensor = tf.keras.preprocessing.sequence.pad_sequences(target_tensor, 
+                                                                  maxlen=max_length_tar, 
+                                                                  padding='post')
+    
+    return input_tensor, target_tensor, inp_lang, targ_lang, max_length_inp, max_length_tar
+	
+input_tensor, target_tensor, inp_lang, targ_lang, max_length_inp, max_length_targ = load_dataset()
 
+from sklearn.model_selection import train_test_split
 
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# Show length
+len(input_tensor_train), len(target_tensor_train), len(input_tensor_val), len(target_tensor_val)
+
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 4
+N_BATCH = BUFFER_SIZE//BATCH_SIZE
+embedding_dim = 256
+units = 256
+vocab_inp_size = len(inp_lang.word2idx)
+vocab_tar_size = len(targ_lang.word2idx)
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
