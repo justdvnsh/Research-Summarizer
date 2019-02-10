d8801ccde2b304aaaeeefcc5badcd345bd4e778e
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2019-02-10 15:49:28 +0530

    modified model

--- model.py
+++ model.py
@@ -5,91 +5,119 @@ Created on Fri Jan 18 11:57:06 2019
 @author: Divyansh
 """
 
-from data_pre_processing import *
-import tensorflow as tf
+from keras.layers import *
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
 import numpy as np
+import pandas as pd
 
-def gru(units):
-  # If you have a GPU, we recommend using CuDNNGRU(provides a 3x speedup than GRU)
-  # the code automatically does that.
-  if tf.test.is_gpu_available():
-    return tf.keras.layers.CuDNNGRU(units, 
-                                    return_sequences=True, 
-                                    return_state=True, 
-                                    recurrent_initializer='glorot_uniform')
-  else:
-    return tf.keras.layers.GRU(units, 
-                               return_sequences=True, 
-                               return_state=True, 
-                               recurrent_activation='sigmoid', 
-                               recurrent_initializer='glorot_uniform')
-							   
-class Encoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
-        super(Encoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.enc_units = enc_units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = gru(self.enc_units)
-        
-    def call(self, x, hidden):
-        x = self.embedding(x)
-        output, state = self.gru(x, initial_state = hidden)        
-        return output, state
-    
-    def initialize_hidden_state(self):
-        return tf.zeros((self.batch_sz, self.enc_units))
-		
-class Decoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
-        super(Decoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.dec_units = dec_units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = gru(self.dec_units)
-        self.fc = tf.keras.layers.Dense(vocab_size)
-        
-        # used for attention
-        self.W1 = tf.keras.layers.Dense(self.dec_units)
-        self.W2 = tf.keras.layers.Dense(self.dec_units)
-        self.V = tf.keras.layers.Dense(1)
-        
-    def call(self, x, hidden, enc_output):
-        # enc_output shape == (batch_size, max_length, hidden_size)
-        
-        # hidden shape == (batch_size, hidden size)
-        # hidden_with_time_axis shape == (batch_size, 1, hidden size)
-        # we are doing this to perform addition to calculate the score
-        hidden_with_time_axis = tf.expand_dims(hidden, 1)
-        
-        # score shape == (batch_size, max_length, hidden_size)
-        score = tf.nn.tanh(self.W1(enc_output) + self.W2(hidden_with_time_axis))
-        
-        # attention_weights shape == (batch_size, max_length, 1)
-        # we get 1 at the last axis because we are applying score to self.V
-        attention_weights = tf.nn.softmax(self.V(score), axis=1)
-        
-        # context_vector shape after sum == (batch_size, hidden_size)
-        context_vector = attention_weights * enc_output
-        context_vector = tf.reduce_sum(context_vector, axis=1)
-        
-        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
-        x = self.embedding(x)
-        
-        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
-        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
-        
-        # passing the concatenated vector to the GRU
-        output, state = self.gru(x)
-        
-        # output shape == (batch_size * 1, hidden_size)
-        output = tf.reshape(output, (-1, output.shape[2]))
-        
-        # output shape == (batch_size * 1, vocab)
-        x = self.fc(output)
-        
-        return x, state, attention_weights
-        
-    def initialize_hidden_state(self):
-        return tf.zeros((self.batch_sz, self.dec_units))
-		
+df = pd.read_csv('dataset.csv')
+print(df.head())
+
+clean_news = [news for news in df['news']]
+clean_summaries = ['<SOS> ' + summary + ' <EOS>' for summary in df['summary']]
+
+# Vectorize the data.
+input_texts = []
+target_texts = []
+input_characters = set()
+target_characters = set()
+for news in clean_news:
+    input_text = news
+    for summary in clean_summaries:
+        target_text = summary
+
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = '\t' + target_text + '\n'
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+    for char in input_text:
+        if char not in input_characters:
+            input_characters.add(char)
+    for char in target_text:
+        if char not in target_characters:
+            target_characters.add(char)
+
+input_characters = sorted(input_characters)
+
+target_characters = sorted(target_characters)
+
+num_encoder_tokens = len(input_characters)
+
+num_decoder_tokens = len(target_characters)
+
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+print('Number of samples:', len(input_texts))
+
+print('Number of unique input tokens:', num_encoder_tokens)
+
+print('Number of unique output tokens:', num_decoder_tokens)
+
+print('Max sequence length for inputs:', max_encoder_seq_length)
+
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+
+latent_dim = 512
+BATCH_SIZE = 32
+epochs = 1
+
+input_token_index = dict(
+    [(char, i) for i, char in enumerate(input_characters)])
+target_token_index = dict(
+    [(char, i) for i, char in enumerate(target_characters)])
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Run training
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=BATCH_SIZE,
+          epochs=epochs,
+          validation_split=0.2)
